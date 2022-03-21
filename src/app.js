388a610a2108eadcdee0d710162bdb014c596fc5
long2512 <buidanglong02@gmail.com>
2022-03-22 02:49:07 +0700

    B5

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productsRouter from './router/products';
+import categoryRouter from './router/category';
 import cors from 'cors';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
@@ -17,6 +18,7 @@ app.use(express.json())
 
 //router
 app.use("/api",productsRouter)
+app.use("/api",categoryRouter)
 
 // connecttion db
 mongoose.connect("mongodb://localhost:27017/we16310")
@@ -24,7 +26,7 @@ mongoose.connect("mongodb://localhost:27017/we16310")
 .catch(() => console.log(error))
 
 //connect
-const PORT = 8000;
+const PORT = 4000;
 app.listen(PORT,()=>{
     console.log('Server đang chạy cổng ',PORT);
 });
